56f0dba2f3c551ed97db38d82590313e9959d171
xmerlinlor <xmerlinlordbot@gmail.com>
2026-09-10 19:38:03 +0100

    Create commands.js

---
+++ commands.js
@@ -0,0 +1,21 @@
+const commands = {
+  OWNER: [],
+  AUTO: [],
+  ANTI: [],
+  GAME: [],
+  STICKER: [],
+  VV: [],
+  GENERAL: [],
+  MODERATOR: [],
+  MUSIC: [],
+  DOWNLOAD: [],
+  ANIME: [],
+  ADMIN: [],
+  LOCK: [],
+  SECURITY: [],
+  TOOLS: [],
+  UTILITY: [],
+  PREMIUM: []
+};
+
+export default commands;
