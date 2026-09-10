d1088725bc224c1c8e8e6911dd3ad26a9ae358a9
xmerlinlor <xmerlinlordbot@gmail.com>
2026-09-10 10:05:20 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+export default {
+  BOT_NAME: "ᴅᴇᴀᴅ × ʙᴏᴛ",
+
+  PREFIX: ".",
+
+  SESSION_DIR: "./session",
+
+  MODE: "public",
+
+  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+
+  PORT: process.env.PORT || 3000
+};
